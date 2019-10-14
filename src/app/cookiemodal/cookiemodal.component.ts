1e6f34d90833c990828984c768bf367cafdfe5b5
kovacszsolt <smith.zsolt@gmail.com>
2019-10-14 22:11:12 +0200

    cookie

---
+++ src/app/cookiemodal/cookiemodal.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit } from '@angular/core';
+import {MatDialogRef} from '@angular/material/dialog';
+
+@Component({
+  selector: 'app-cookiemodal',
+  templateUrl: './cookiemodal.component.html',
+  styleUrls: ['./cookiemodal.component.scss']
+})
+export class CookiemodalComponent implements OnInit {
+
+  constructor(public dialogRef: MatDialogRef<CookiemodalComponent>) { }
+
+  ngOnInit() {
+  }
+
+}
